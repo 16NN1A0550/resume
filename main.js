bfa9d662eb5d1a81b222ec22bbea2488aa43583a
16NN1A0550 <42430055+16NN1A0550@users.noreply.github.com>
2018-08-19 12:11:27 +0530

    adding all files

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function loadjson(file){
-	return new Promise((reslove,reject)=>{
+	return new Promise((resolve,reject)=>{
 		return fetch(file).then(res=>{
 			if(res.ok){
-				reslove(res.json());
+				resolve(res.json());
 			}
 			else{
 				reject(new Error('error'));
@@ -14,6 +14,8 @@ var fetchedData=loadjson("data.json");
 fetchedData.then(data=>{
 	console.log(data);
 	career(data.career);
+	education(data.education);
+	skills(data.skills);
 })
 var childTwo=document.querySelector(".child2");
 console.log(childTwo);
@@ -23,4 +25,51 @@ function career(car)
 	var heading=document.createElement("h3");
 	heading.textContent="career objective";
 	childTwo.append(heading);
+	var hr=document.createElement("hr");
+	heading.append(hr);
+	var text=document.createElement("p");
+	text.textContent=car.info;
+	childTwo.append(text);
+}
+function education(edu)
+{
+	var heading=document.createElement("h3");
+	heading.textContent="education qualification";
+	childTwo.append(heading);
+	var hr=document.createElement("hr");
+	heading.append(hr);
+	var tab=document.createElement("table");
+	tab.border="1";
+	childTwo.append(tab);
+	var tr="<tr> <th>Degree</th> <th>Institute</th> <th>Data</td></th>";
+	//tab.innerHTML=tr;
+	var tr1="";
+	for(var i=0;i<edu.length;i++){
+     tr1+="<tr> <td> "+edu[i].degree+"</td> <td>"+edu[i].institute+"</td> <td>"+edu[i].Data+" </td></tr>"
+}     	
+tab.innerHTML=tr+tr1;
+}
+function skills(sk){
+	var heading=document.createElement("h3");
+	heading.textContent="Technical Skills";
+	childTwo.append(heading);
+	var hr=document.createElement("hr");
+	heading.append(hr);
+	for(var i=0;i<sk.length;i++){
+		var skdiv=document.createElement("div");
+        skdiv.classList.add("skdiv");
+        childTwo.append(skdiv);
+        var title=document.createElement("h3");
+        title.textContent=sk[i].title;
+        skdiv.append(title);
+        var hr=document.createElement("hr");
+	    title.append(hr);
+	    for(var j=0;j<sk[i].content.length;j++){
+	    	var con=document.createElement("span");
+	        con.textContent=sk[i].content[j];
+	        skdiv.append(con);
+	    }
+
+	}
+
 }
